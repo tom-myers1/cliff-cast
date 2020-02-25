ec2e36caad401b5607aa7500e8bc8288efdae89a
Tom Myers <tom.myers@Zolv.com>
2020-02-25 16:53:46 +0000

    added listeners for api

--- main.go
+++ main.go
@@ -4,10 +4,16 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"strconv"
 	"time"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 )
+
+var d1, d2, d3 Final
 
 // R1 is top level struct
 type R1 struct {
@@ -51,14 +57,14 @@ type Res struct {
 func check(e error) {
 	if e != nil {
 		fmt.Println("an error occured... panic!!!")
-		panic(e)
+		log.Fatal(e)
 	}
 }
 
 // getInfo calls endpoint and returns []byte
 func getInfo(url string) []byte {
 
-	res, err := http.Get("http://ws1.metcheck.com/ENGINE/v9_0/json.asp?lat=53.9&lon=-1.6&lid=67633&Fc=No")
+	res, err := http.Get(url)
 	check(err)
 	info, err := ioutil.ReadAll(res.Body)
 	res.Body.Close()
@@ -69,13 +75,13 @@ func getInfo(url string) []byte {
 }
 
 // unmarshal sorts through json to filter what is needed
-func unmarshal(info []byte) {
+func unmarshal(info []byte) []Res {
 	// to struct
 	var data R1
 	err := json.Unmarshal(info, &data)
 	check(err)
 	fCast := data.D1.Location.Forecast
-	forecast(fCast)
+	return fCast
 }
 
 // MinMax returns the min and max values frm int64 slice
@@ -93,13 +99,22 @@ func MinMax(array []int64) (int64, int64) {
 	return min, max
 }
 
-// quickPrint just displays to console
-func quickPrint(f Final) {
+// quickPrint just displays to console (and now returns Final)
+func quickPrint(f Final) Final {
+	var r Final
 	rmin, rmax := MinMax(f.RainChance)
 	tmin, tmax := MinMax(f.Temp)
 	wmin, wmax := MinMax(f.Wind)
 	gmin, gmax := MinMax(f.Gust)
 	hmin, hmax := MinMax(f.Humid)
+
+	r.Day = f.Day
+	r.RainTotal = f.RainTotal // math.Round(f.RainTotal) - rounds to whole?!?!?
+	r.RainChance = append(r.RainChance, rmin, rmax)
+	r.Temp = append(r.Temp, tmin, tmax)
+	r.Wind = append(r.Wind, wmin, wmax)
+	r.Gust = append(r.Gust, gmin, gmax)
+	r.Humid = append(r.Humid, hmin, hmax)
 
 	fmt.Println(f.Day)
 	fmt.Printf("chance of rain between %d percent and %d percent\n", rmin, rmax)
@@ -108,13 +123,14 @@ func quickPrint(f Final) {
 	fmt.Printf("wind speed between %dmph and %dmph\n", wmin, wmax)
 	fmt.Printf("gusts between %d and %d\n", gmin, gmax)
 	fmt.Printf("humidity between %d and %d\n", hmin, hmax)
+	return r
 }
 
 // forecast checks through forecast for next 7 days
-func forecast(fCast []Res) {
+func forecast(fCast []Res) (Final, Final, Final) {
 
 	var tDay string
-	var d1, d2, d3 Final
+	//var d1, d2, d3 Final
 	loc, _ := time.LoadLocation("UTC")
 	layout := "2006-01-02T15:04:05"
 
@@ -179,15 +195,46 @@ func forecast(fCast []Res) {
 
 	}
 
-	quickPrint(d1)
-	quickPrint(d2)
-	quickPrint(d3)
+	d1 = quickPrint(d1)
+	d2 = quickPrint(d2)
+	d3 = quickPrint(d3)
+	return d1, d2, d3
+
+}
+
+// Call returns data
+func Call(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, d1, d2, d3)
+}
+
+// Refresh resfreshes data
+func Refresh(w http.ResponseWriter, r *http.Request) {
+	Start()
+	fmt.Fprint(w, "refreshing!")
+}
+
+// GetURL posts URL
+func GetURL(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "expects to be passed a URL") // future proofing to allow for more locations to be added
+}
+
+// Start is used to get info and start passing info about
+func Start() {
+
+	url := "http://ws1.metcheck.com/ENGINE/v9_0/json.asp?lat=53.9&lon=-1.6&lid=67633&Fc=No"
+	info := getInfo(url)
+	unmarsh := unmarshal(info)
+	forecast(unmarsh)
 
 }
 
 func main() {
-	url := "http://ws1.metcheck.com/ENGINE/v9_0/json.asp?lat=53.9&lon=-1.6&lid=67633&Fc=No"
-	info := getInfo(url)
-	unmarshal(info) // unmarshall to struct
+	Start()
+	r := mux.NewRouter().StrictSlash(true)
+	r.HandleFunc("/call", Call).Methods("GET")
+	r.HandleFunc("/refresh", Refresh).Methods("GET")
+	r.HandleFunc("/Url", GetURL).Methods("POST")
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":8080", handlers.CORS(handlers.AllowedMethods([]string{"GET", "POST"}), handlers.AllowedOrigins([]string{"*"}))(r)))
 
 }
